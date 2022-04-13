3e9b4932dfa50992c3e151bcee858ed8de32fa97
NorthSeaPark <hou.308@osu.edu>
2022-04-13 11:54:21 +0800

    go

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -1,16 +1,16 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
     # 管理游戏资源和行为的类
     def __init__(self):
-
+        # 初始化游戏并创建游戏资源
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        # 设置背景色
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # 开始游戏的主循环
@@ -20,7 +20,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # 让最近绘制的屏幕可见
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 
