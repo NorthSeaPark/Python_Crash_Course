6b6004e6df6bd377e1b65edc3617d45a10f4f804
NorthSeaPark <hou.308@osu.edu>
2022-04-15 11:32:51 +0800

    go

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -1,50 +1,90 @@
 import sys
+
 import pygame
+
 from settings import Settings
 from ship import Ship
-
+from bullet import Bullet
 
 class AlienInvasion:
-    # 管理游戏资源和行为的类
+    """Overall class to manage game assets and behavior."""
+
     def __init__(self):
-        # 初始化游戏并创建游戏资源
+        """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
-        # 开始游戏的主循环
+        """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
+            self._update_bullets()
             self._update_screen()
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
 
     def _check_events(self):
+        """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                 self.bullets.remove(bullet)
 
     def _update_screen(self):
-        # 每次循环时都重绘屏幕
+        """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
-        self.ship.update()
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
-    # 创建游戏实例并运行
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
--- 外星人入侵/ship.py
+++ 外星人入侵/ship.py
@@ -2,32 +2,39 @@ import pygame
 
 
 class Ship:
-    def __init__(self, ai_game):
-        # 初始化飞船并设置其初始位置
-        self.screen = ai_game.screen
-        self.settings = ai_game.setttings
+    """A class to manage the ship."""
 
-        # 加载飞船图像并获取其外接矩阵
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        # 对于每艘新飞船，都将其放在屏幕底部的中央
+        # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
-        # 移动标志
+        # Movement flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
-            self.rect.x += self.settings.ship_speed
-        if self.moving_left:
-            self.rect.x -= self.settings.ship_speed
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
 
+        # Update rect object from self.x.
         self.rect.x = self.x
 
     def blitme(self):
-        # 在指定位置绘制飞船
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
