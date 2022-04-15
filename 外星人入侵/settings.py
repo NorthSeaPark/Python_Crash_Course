d5018a1514339c468efb5422d9b3b39b4b4a430c
NorthSeaPark <hou.308@osu.edu>
2022-04-15 11:10:14 +0800

    go

--- 外星人入侵/settings.py
+++ 外星人入侵/settings.py
@@ -4,4 +4,3 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
-        
--- 外星人入侵/ship.py
+++ 外星人入侵/ship.py
@@ -1,10 +1,11 @@
 import pygame
-class Ship:
 
+
+class Ship:
     def __init__(self, ai_game):
         # 初始化飞船并设置其初始位置
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.setttings
 
         # 加载飞船图像并获取其外接矩阵
         self.image = pygame.image.load('images/ship.bmp')
@@ -13,15 +14,19 @@ class Ship:
         # 对于每艘新飞船，都将其放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.rect.x)
+
         # 移动标志
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.rect.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.rect.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         # 在指定位置绘制飞船
