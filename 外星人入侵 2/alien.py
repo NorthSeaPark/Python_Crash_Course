8cd69d449f6153075a2418fa93bbed7e35ff15a8
NorthSeaPark <hou.308@osu.edu>
2022-04-17 10:48:16 +0800

    shut up

---
+++ 外星人入侵 2/alien.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    # 表示单个外星人
+    def __init__(self, ai_game):
+        # 初始化外星人并设置起始位置
+        super().__init__()
+        self.screen = ai_game.screen
+        # 加载图形并设置rect属性
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
