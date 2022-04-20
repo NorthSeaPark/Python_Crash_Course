31f49911d059e5980dcff23042ca95aea885aa66
NorthSeaPark <hou.308@osu.edu>
2022-04-20 11:49:22 +0800

    go

--- 数据可视化/随机漫步/random_walk.py
+++ 数据可视化/随机漫步/random_walk.py
@@ -12,3 +12,16 @@ class RandomWalk:
         self.y_values = [0]
 
     def fill_walk(self):
+        # 计算随机漫步包含的所有的点
+
+        # 不断漫步，直到列表达到指定的长度
+        while len(self.x_values) < self.num_points:
+            # 决定前进的方向与前进的距离
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+            
