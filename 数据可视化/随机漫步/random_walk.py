9542a1459a854fb492f518c9168a5d1918efe049
NorthSeaPark <hou.308@osu.edu>
2022-04-20 11:41:51 +0800

    go

---
+++ 数据可视化/随机漫步/random_walk.py
@@ -0,0 +1,14 @@
+from random import choice
+
+
+class RandomWalk:
+
+    def __init__(self, num_points=5000):
+        # 初始化随机漫步的属性
+        self.num_points = num_points
+
+        # 所有随机漫步都始于(0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
