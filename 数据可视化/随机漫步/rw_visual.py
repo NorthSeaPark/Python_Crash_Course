86f2858c1dfb3bf59be9c157cd07b13c0b219043
NorthSeaPark <hou.308@osu.edu>
2022-04-21 10:59:10 +0800

    go

--- 数据可视化/随机漫步/rw_visual.py
+++ 数据可视化/随机漫步/rw_visual.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# 创建一个Random Walk实例
-rw = RandomWalk()
-rw.fill_walk()
-# 将所有点都绘制出来
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# 不断地模拟随机漫步
+while True:
+    # 创建一个Random Walk实例
+    rw = RandomWalk()
+    rw.fill_walk()
+    # 将所有点都绘制出来
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    # 随机漫步从浅色变成深色
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
 
